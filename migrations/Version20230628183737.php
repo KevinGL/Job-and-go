11736413facc22070d22702570854a38cd3d1187
kevin <kevin.progc@gmail.com>
2023-06-28 20:53:55 +0200

    Projet de base, sans users pour le moment, avec une table listantles candidatures

---
+++ migrations/Version20230628183737.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20230628183737 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE candidacy (id INT AUTO_INCREMENT NOT NULL, society VARCHAR(255) NOT NULL, type VARCHAR(255) NOT NULL, candidacy_date DATE NOT NULL, relaunch_date DATE DEFAULT NULL, comments LONGTEXT DEFAULT NULL, issue VARCHAR(255) DEFAULT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('DROP TABLE candidacy');
+    }
+}
---
+++ src/Entity/Candidacy.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CandidacyRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CandidacyRepository::class)]
+class Candidacy
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $society = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $candidacy_date = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $relaunch_date = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $comments = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $issue = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSociety(): ?string
+    {
+        return $this->society;
+    }
+
+    public function setSociety(string $society): static
+    {
+        $this->society = $society;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getCandidacyDate(): ?\DateTimeInterface
+    {
+        return $this->candidacy_date;
+    }
+
+    public function setCandidacyDate(\DateTimeInterface $candidacy_date): static
+    {
+        $this->candidacy_date = $candidacy_date;
+
+        return $this;
+    }
+
+    public function getRelaunchDate(): ?\DateTimeInterface
+    {
+        return $this->relaunch_date;
+    }
+
+    public function setRelaunchDate(?\DateTimeInterface $relaunch_date): static
+    {
+        $this->relaunch_date = $relaunch_date;
+
+        return $this;
+    }
+
+    public function getComments(): ?string
+    {
+        return $this->comments;
+    }
+
+    public function setComments(?string $comments): static
+    {
+        $this->comments = $comments;
+
+        return $this;
+    }
+
+    public function getIssue(): ?string
+    {
+        return $this->issue;
+    }
+
+    public function setIssue(?string $issue): static
+    {
+        $this->issue = $issue;
+
+        return $this;
+    }
+}
