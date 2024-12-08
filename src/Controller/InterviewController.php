ffc863aadb0e8de283cd360fa38b397a706c5d68
kevin <kevin.progc@gmail.com>
2024-12-08 18:07:33 +0100

    Ajout entretiens de recrutement

---
+++ src/Controller/InterviewController.php
@@ -0,0 +1,364 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\InterviewRepository;
+use App\Entity\Interview;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+
+class InterviewController extends AbstractController
+{
+    #[Route('/interview', name: 'app_interview')]
+    public function index(InterviewRepository $repo): Response
+    {
+        $queryBuilder = $repo->createQueryBuilder("i");
+
+        $queryBuilder->orderBy("i.date", "DESC");
+        
+        $interviews = $queryBuilder->getQuery()->getResult();
+
+        $this->getNeedRelaunch($interviews);
+        
+        return $this->render('interview/index.html.twig',
+        [
+            'interviews' => $interviews,
+        ]);
+    }
+
+    #[Route('/interview/add', name: 'add_interview')]
+    public function add(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $interview = new interview();
+        
+        $form = $this->createFormBuilder($interview)
+
+        ->add("society", TextType::class,
+        [
+            "attr" =>
+            [
+                "class" => "form-control"
+            ],
+            "label" => "Nom de la société"
+        ])
+
+        ->add("type", ChoiceType::class,
+        [
+            "attr" =>
+            [
+                "class" => "form-select"
+            ],
+            "choices" =>
+            [
+                "France Travail" => "France Travail",
+                "Indeed" => "Indeed",
+                "Hello Work" => "Hello Work",
+                "Welcome to the jungle" => "Welcome to the jungle",
+                "Meteo Job" => "Meteo Job",
+                "LinkedIn" => "LinkedIn",
+                "La Bonne Boîte" => "La Bonne Boîte",
+                "La Bonne Alternance" => "La Bonne Alternance",
+                "Spontané" => "Spontané",
+                "Interim" => "Interim",
+                "Autre" => "Autre"
+            ],
+            "label" => "Type de candidature"
+        ])
+
+        ->add("searchedContract", ChoiceType::class,
+        [
+            "attr" =>
+            [
+                "class" => "form-select"
+            ],
+            "choices" =>
+            [
+                "CDD/CDI" => "CDD/CDI",
+                "Stage" => "Stage",
+                "Alternance" => "Alternance",
+                "Autre" => "Autre"
+            ],
+            "label" => "Type de contrat recherché"
+        ])
+
+        ->add("date", DateType::class,
+        [
+            "widget" => "single_text",
+            "label" => "Date de l'entretien",
+            "attr" =>
+            [
+                "class" => "form-control form-icon-trailing"
+            ],
+            "data" => new \DateTimeImmutable()
+        ])
+
+        ->add("job", TextType::class,
+        [
+            "attr" =>
+            [
+                "class" => "form-control"
+            ],
+            "label" => "Poste proposé / recherché"
+        ])
+
+        ->add("comments", TextareaType::class,
+        [
+            "attr" =>
+            [
+                "class" => "form-control"
+            ],
+            "label" => "Commentaires",
+            "required" => false
+        ])
+
+        ->getForm();
+        
+        $form->handleRequest($request);
+        
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager->persist($interview);
+
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_interview');
+        }
+        
+        return $this->render('interview/add.html.twig', ["form" => $form]);
+    }
+
+    #[Route('/interview/view/{id}', name: 'view_interview')]
+    public function view($id, InterviewRepository $repo): Response
+    {
+        $int = $repo->find($id);
+
+        $this->getNeedRelaunchOne($int);
+
+        return $this->render('interview/view.html.twig', ["int" => $int]);
+    }
+
+    #[Route('/interview/edit/{id}', name: 'edit_interview')]
+    public function edit($id, Request $request, InterviewRepository $repo, EntityManagerInterface $entityManager): Response
+    {
+        $int = $repo->find($id);
+
+        $form = $this->createFormBuilder($int)
+
+        ->add("society", TextType::class,
+        [
+            "attr" =>
+            [
+                "class" => "form-control"
+            ],
+            "label" => "Nom de la société"
+        ])
+
+        ->add("type", ChoiceType::class,
+        [
+            "attr" =>
+            [
+                "class" => "form-select"
+            ],
+            "choices" =>
+            [
+                "France Travail" => "France Travail",
+                "Indeed" => "Indeed",
+                "Hello Work" => "Hello Work",
+                "Welcome to the jungle" => "Welcome to the jungle",
+                "Meteo Job" => "Meteo Job",
+                "LinkedIn" => "LinkedIn",
+                "La Bonne Boîte" => "La Bonne Boîte",
+                "La Bonne Alternance" => "La Bonne Alternance",
+                "Spontané" => "Spontané",
+                "Interim" => "Interim",
+                "Autre" => "Autre"
+            ],
+            "label" => "Type de candidature"
+        ])
+
+        ->add("searchedContract", ChoiceType::class,
+        [
+            "attr" =>
+            [
+                "class" => "form-select"
+            ],
+            "choices" =>
+            [
+                "CDD/CDI" => "CDD/CDI",
+                "Stage" => "Stage",
+                "Alternance" => "Alternance",
+                "Autre" => "Autre"
+            ],
+            "label" => "Type de contrat recherché"
+        ])
+
+        ->add("date", DateType::class,
+        [
+            "widget" => "single_text",
+            "label" => "Date de candidature",
+            "attr" =>
+            [
+                "class" => "form-control form-icon-trailing"
+            ]
+        ])
+
+        ->add("relaunch_date", DateType::class,
+        [
+            "widget" => "single_text",
+            "label" => "Date de relance",
+            "attr" =>
+            [
+                "class" => "form-control form-icon-trailing"
+            ],
+            "required" => false
+        ])
+
+        ->add("comments", TextareaType::class,
+        [
+            "attr" =>
+            [
+                "class" => "form-control"
+            ],
+            "label" => "Commentaires",
+            "required" => false
+        ])
+
+        ->add("issue", ChoiceType::class,
+        [
+            "attr" =>
+            [
+                "class" => "form-select"
+            ],
+            "choices" =>
+            [
+                "Pas de réponse pour le moment" => null,
+                "Poste décroché :)" => "ok",
+                "Réponse négative :(" => "no"
+            ],
+            "label" => "Issue de la candidature"
+        ])
+        
+        ->getForm();
+        
+        $form->handleRequest($request);	
+        
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager->persist($int);
+            $entityManager->flush();
+
+            return $this->redirectToRoute("app_interview");
+        }
+        
+        return $this->render('interview/edit.html.twig', ["form" => $form]);
+    }
+
+    #[Route('/interview/delete/{id}', name: 'delete_interview')]
+    public function delete($id, InterviewRepository $repo, EntityManagerInterface $entityManager): Response
+    {
+        $int = $repo->find($id);
+
+        $entityManager->remove($int);
+        $entityManager->flush();
+
+        return $this->redirectToRoute("app_interview");
+    }
+
+    private function getNeedRelaunch(&$interview)
+    {
+        foreach($interview as $int)
+        {
+            if($int->getRelaunchDate() == null && $int->getIssue() == null)
+            {
+                $now = new \DateTimeImmutable();
+
+                $delay = $now->diff($int->getDate())->format("%a");
+
+                if($delay < 7)
+                {
+                    $int->needRelaunch = "#5bfe6a";
+                }
+
+                else
+                if($delay >= 7 && $delay < 14)
+                {
+                    $int->needRelaunch = "yellow";
+                }
+
+                else
+                if($delay >= 14 && $delay < 21)
+                {
+                    $int->needRelaunch = "#f9a33e";
+                }
+
+                else
+                if($delay >= 21 && $delay < 28)
+                {
+                    $int->needRelaunch = "red";
+                }
+
+                else
+                if($delay >= 28)
+                {
+                    $int->needRelaunch = "black";
+                }
+            }
+
+            else
+            {
+                $int->needRelaunch = "white";
+            }
+        }
+    }
+
+    private function getNeedRelaunchOne(&$int)
+    {
+        if($int->getRelaunchDate() == null && $int->getIssue() == null)
+        {
+            $now = new \DateTimeImmutable();
+
+            $delay = $now->diff($int->getDate())->format("%a");
+
+            if($delay < 7)
+            {
+                $int->needRelaunch = "#5bfe6a";
+            }
+
+            else
+            if($delay >= 7 && $delay < 14)
+            {
+                $int->needRelaunch = "yellow";
+            }
+
+            else
+            if($delay >= 14 && $delay < 21)
+            {
+                $int->needRelaunch = "#f9a33e";
+            }
+
+            else
+            if($delay >= 21 && $delay < 28)
+            {
+                $int->needRelaunch = "red";
+            }
+
+            else
+            if($delay >= 28)
+            {
+                $int->needRelaunch = "black";
+            }
+        }
+
+        else
+        {
+            $int->needRelaunch = "white";
+        }
+    }
+}
---
+++ src/Entity/Interview.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\InterviewRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: InterviewRepository::class)]
+class Interview
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $society = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $comments = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $type = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE, nullable: true)]
+    private ?\DateTimeInterface $relaunchDate = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $issue = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $searchedContract = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $job = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getSociety(): ?string
+    {
+        return $this->society;
+    }
+
+    public function setSociety(string $society): static
+    {
+        $this->society = $society;
+
+        return $this;
+    }
+
+    public function getComments(): ?string
+    {
+        return $this->comments;
+    }
+
+    public function setComments(?string $comments): static
+    {
+        $this->comments = $comments;
+
+        return $this;
+    }
+
+    public function getType(): ?string
+    {
+        return $this->type;
+    }
+
+    public function setType(string $type): static
+    {
+        $this->type = $type;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): static
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getRelaunchDate(): ?\DateTimeInterface
+    {
+        return $this->relaunchDate;
+    }
+
+    public function setRelaunchDate(\DateTimeInterface $relaunchDate): static
+    {
+        $this->relaunchDate = $relaunchDate;
+
+        return $this;
+    }
+
+    public function getIssue(): ?string
+    {
+        return $this->issue;
+    }
+
+    public function setIssue(?string $issue): static
+    {
+        $this->issue = $issue;
+
+        return $this;
+    }
+
+    public function getSearchedContract(): ?string
+    {
+        return $this->searchedContract;
+    }
+
+    public function setSearchedContract(string $searchedContract): static
+    {
+        $this->searchedContract = $searchedContract;
+
+        return $this;
+    }
+
+    public function getJob(): ?string
+    {
+        return $this->job;
+    }
+
+    public function setJob(string $job): static
+    {
+        $this->job = $job;
+
+        return $this;
+    }
+}
